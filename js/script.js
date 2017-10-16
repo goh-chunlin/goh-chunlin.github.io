d6133b5107a024dde331777745425e6a6dc1e2d0
Goh Chun Lin <chunlin@live.com.sg>
2017-10-16 22:07:43 +0800

    Fixed the issue of homepage shaking on some screen resolutions.

--- js/script.js
+++ js/script.js
@@ -133,8 +133,9 @@ $(document).ready(function() {
 
         "use strict";
         
-        var widnowHeight = $(window).height();
-        var containerHeight = $(".home-container").height();
+        var widnowHeight = parseInt($(window).height());
+        var containerHeight = parseInt($(".home-container").height());
+	containerHeight = (containerHeight % 2 == 0) ? containerHeight : containerHeight + 1;
         var padTop = widnowHeight - containerHeight;
         $(".home-container").css({
             'padding-top': Math.round(padTop / 2) + 'px',
